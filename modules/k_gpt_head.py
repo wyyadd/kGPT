ec776aeb94884b40e0bcc072dee4cb23b7eac06d
wyyadd <wyyadd@gmail.com>
2025-01-14 16:19:27 -0800

    feat: update head

--- modules/k_gpt_head.py
+++ modules/k_gpt_head.py
@@ -33,40 +33,25 @@ class KGPTHead(nn.Module):
         self.apply(weight_init)
 
     def forward(self, x_a: torch.Tensor) -> Dict[str, torch.Tensor]:
+        # [agents, steps, patch, dim]
+        x_a = x_a.transpose(-1, -2)
+
         # [agents, steps, patch, modes]
-        pi = self.to_pi(x_a.transpose(-2, -1))
+        pi = self.to_pi(x_a)
+        control_action = self.to_control_action(x_a)
+        scale = self.to_scale(x_a)
 
-        acc = torch.tensor([], device=x_a.device)
-        delta = torch.tensor([], device=x_a.device)
-        acc_scale = torch.tensor([], device=x_a.device)
-        delta_scale = torch.tensor([], device=x_a.device)
-        height = torch.tensor([], device=x_a.device)
-        height_scale = torch.tensor([], device=x_a.device)
-
-        for i in range(self.patch_size):
-            h = x_a[..., i]
-            control_action = self.to_control_action(h).unsqueeze(-2)
-            scale = self.to_scale(h).unsqueeze(-2)
-
-            new_acc, new_delta, new_height = control_action.split([self.acc_dim * self.num_modes,
-                                                                   self.delta_dim * self.num_modes,
-                                                                   self.height_dim * self.num_modes], dim=-1)
-            new_acc_scale, new_delta_scale, new_height_scale = scale.split([self.acc_dim * self.num_modes,
-                                                                            self.delta_dim * self.num_modes,
-                                                                            self.height_dim * self.num_modes], dim=-1)
-
-            # constrain to [-pi,pi] same to target
-            new_delta = torch.tanh(new_delta) * math.pi
-            new_acc_scale = F.elu(new_acc_scale, alpha=1.0) + 1.0
-            new_delta_scale = 1.0 / (F.elu(new_delta_scale, alpha=1.0) + 1.0 + 1e-4)
-            new_height_scale = F.elu(new_height_scale, alpha=1.0) + 1.0
-
-            acc = torch.cat([acc, new_acc], dim=-2)
-            delta = torch.cat([delta, new_delta], dim=-2)
-            height = torch.cat([height, new_height], dim=-2)
-            acc_scale = torch.cat([acc_scale, new_acc_scale], dim=-2)
-            delta_scale = torch.cat([delta_scale, new_delta_scale], dim=-2)
-            height_scale = torch.cat([height_scale, new_height_scale], dim=-2)
+        acc, delta, height = control_action.split([self.acc_dim * self.num_modes,
+                                                   self.delta_dim * self.num_modes,
+                                                   self.height_dim * self.num_modes], dim=-1)
+        acc_scale, delta_scale, height_scale = scale.split([self.acc_dim * self.num_modes,
+                                                            self.delta_dim * self.num_modes,
+                                                            self.height_dim * self.num_modes], dim=-1)
+        # constrain to [-pi,pi] same to target
+        delta = torch.tanh(delta) * math.pi
+        acc_scale = F.elu(acc_scale, alpha=1.0) + 1.0
+        delta_scale = 1.0 / (F.elu(delta_scale, alpha=1.0) + 1.0 + 1e-4)
+        height_scale = F.elu(height_scale, alpha=1.0) + 1.0
 
         acc = acc.reshape(*x_a.shape[:2], -1, self.num_modes, self.acc_dim).transpose(-3, -2)
         delta = delta.reshape(*x_a.shape[:2], -1, self.num_modes, self.delta_dim).transpose(-3, -2)

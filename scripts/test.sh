bd65cdb20e36cb76f4954af7cf0a1927e23b24a9
wyyadd <wyyadd@gmail.com>
2025-01-22 13:17:12 -0800

    feat: update script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=a100:4
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=0-10:00:00
 #SBATCH --mail-user=wyyadd@gmail.com
@@ -17,7 +17,7 @@ srun python3 train_k_gpt.py \
 --mode="val" \
 --root="$project/kGPT/data" \
 --train_processed_dir="$SLURM_TMPDIR/processed" \
---num_workers=$SLURM_CPUS_PER_TASK \
+--num_workers=4 \
 --accelerator="auto" \
 --devices=-1 \
 --num_nodes=$SLURM_NNODES \
--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=a100:4
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=2-12:00:00
 #SBATCH --mail-user=wyyadd@gmail.com
@@ -21,7 +21,7 @@ tar -I pigz -xf $project/kGPT/data/training/processed.tar.gz -C $SLURM_TMPDIR
 srun python3 train_k_gpt.py \
 --root="$project/kGPT/data" \
 --train_processed_dir="$SLURM_TMPDIR/processed" \
---num_workers=$SLURM_CPUS_PER_TASK \
+--num_workers=4 \
 --accelerator="auto" \
 --devices=-1 \
 --num_nodes=$SLURM_NNODES \

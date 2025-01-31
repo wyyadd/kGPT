53823900028325d088ffed09349f0f5af54fa9ad
wyyadd <wyyadd@gmail.com>
2025-01-30 22:55:07 -0800

    feat: update filter

--- transforms/sim_agent_filter.py
+++ transforms/sim_agent_filter.py
@@ -24,37 +24,12 @@ class SimAgentFilter(BaseTransform):
         self.max_num_agents = max_num_agents
 
     def __call__(self, data: HeteroData) -> HeteroData:
-        if data['agent']['num_nodes'] <= self.max_num_agents:
-            data['agent']['target_idx'] = torch.arange(data['agent']['num_nodes'], dtype=torch.long,
-                                                       device=data['agent']['position'].device)
-            return data
-
         pos = data['agent']['position'][..., :2]
         av_index = data['agent']['av_index']
-        valid_mask = data['agent']['valid_mask']
-        target_mask = data['agent']['target_mask'].clone()
-
+        target_mask = data['agent']['target_mask']
         # add sdv and track_to_predict
         agent_inds = pos.new_tensor([av_index], dtype=torch.long)
-        target_mask[av_index] = False
         agent_inds = torch.cat([agent_inds, torch.where(target_mask)[0]], dim=0)
-        max_num_context_agents = self.max_num_agents - agent_inds.numel()
-
-        if max_num_context_agents > 0:
-            # add context agents
-            valid_nums = torch.sum(valid_mask, dim=-1)
-            valid_nums[agent_inds] = 0
-            # for i in range(valid_mask.size(0)):
-            #     if valid_nums[i] > 1:
-            #         max_d = torch.max(torch.norm(pos[i, valid_mask[i]][1:] - pos[i, valid_mask[i]][:-1], p=2, dim=-1))
-            #         if max_d.item() < 0.03:
-            #             valid_nums[i] = 0
-            #     else:
-            #         valid_nums[i] = 0
-            context_agent_inds = torch.multinomial(F.softmax(valid_nums.to(torch.float32), dim=-1),
-                                                   num_samples=max_num_context_agents, replacement=False)
-            agent_inds = torch.cat([agent_inds, context_agent_inds], dim=0)
         agent_inds = torch.unique(agent_inds)
         data['agent']['target_idx'] = agent_inds
-
         return data

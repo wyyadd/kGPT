6836c227577d5b0543e8e21fe740dcaa1e7d5b56
wyyadd <wyyadd@gmail.com>
2025-02-04 16:41:25 -0800

    feat: update filter

--- transforms/sim_agent_filter.py
+++ transforms/sim_agent_filter.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import torch
+import torch.nn.functional as F
 from torch_geometric.data import HeteroData
 from torch_geometric.transforms import BaseTransform
 
@@ -23,12 +24,29 @@ class SimAgentFilter(BaseTransform):
         self.max_num_agents = max_num_agents
 
     def __call__(self, data: HeteroData) -> HeteroData:
+        if data['agent']['num_nodes'] <= self.max_num_agents:
+            data['agent']['target_idx'] = torch.arange(data['agent']['num_nodes'], dtype=torch.long,
+                                                       device=data['agent']['position'].device)
+            return data
+
         pos = data['agent']['position'][..., :2]
         av_index = data['agent']['av_index']
-        target_mask = data['agent']['target_mask']
+        valid_mask = data['agent']['valid_mask']
+        target_mask = data['agent']['target_mask'].clone()
+
         # add sdv and track_to_predict
         agent_inds = pos.new_tensor([av_index], dtype=torch.long)
+        target_mask[av_index] = False
         agent_inds = torch.cat([agent_inds, torch.where(target_mask)[0]], dim=0)
+        max_num_context_agents = self.max_num_agents - agent_inds.numel()
+
+        if max_num_context_agents > 0:
+            # add context agents
+            valid_nums = torch.sum(valid_mask, dim=-1)
+            valid_nums[agent_inds] = 0
+            context_agent_inds = torch.multinomial(F.softmax(valid_nums.to(torch.float32), dim=-1),
+                                                   num_samples=max_num_context_agents, replacement=False)
+            agent_inds = torch.cat([agent_inds, context_agent_inds], dim=0)
         agent_inds = torch.unique(agent_inds)
         data['agent']['target_idx'] = agent_inds
         return data

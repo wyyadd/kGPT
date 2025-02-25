12544e3b6a090a18b68a00aefc07e61dc9adcd47
wyyadd <wyyadd@gmail.com>
2025-02-24 17:49:34 -0800

    exp: update shrinkage

--- layers/shrinkage.py
+++ layers/shrinkage.py
@@ -7,6 +7,7 @@ from utils import weight_init
 class Shrinkage(nn.Module):
     def __init__(self, hidden_dim):
         super(Shrinkage, self).__init__()
+        self.avg = nn.AdaptiveAvgPool1d(1)
         self.fc = nn.Sequential(
             nn.Linear(hidden_dim, hidden_dim),
             nn.RMSNorm(hidden_dim),
@@ -18,8 +19,13 @@ class Shrinkage(nn.Module):
     def forward(self, x):
         x_raw = x
         x_abs = torch.abs(x)
-        alpha = torch.sigmoid(self.fc(x_abs))
-        threshold = torch.mul(x_abs, alpha)
-        sub = x_abs - threshold
-        x = torch.mul(torch.sign(x_raw), torch.max(sub, torch.zeros_like(x)))
+        # avg
+        avg = self.avg(x_abs)
+        # alpha
+        x = torch.sigmoid(self.fc(x_raw))
+        # threshold
+        x = torch.mul(avg, x)
+        # sub
+        x = x_abs - x
+        x = torch.mul(torch.sign(x_raw), torch.max(x, torch.zeros_like(x)))
         return x
